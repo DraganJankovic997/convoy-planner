aaaabfdb7ad415cbbffc2e7d5b8f73e0172e9483
Dragan Jankovic <draganjankovic131@gmail.com>
2022-02-06 17:08:16 +0100

    Added a rest stop table migration

---
+++ convoy-planner/database/migrations/2022_02_06_160412_create_reststops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReststopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reststops', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->integer('work_from');
+            $table->integer('work_to');
+            $table->boolean('work_sunday');
+            $table->boolean('work_saturday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reststops');
+    }
+}
