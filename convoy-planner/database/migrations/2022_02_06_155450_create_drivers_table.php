dcd69a59ff0e105cbbf262667794d963ec2bce89
Dragan Jankovic <draganjankovic131@gmail.com>
2022-02-06 17:01:31 +0100

    Created migrations for users, drivers and dispatchers

--- convoy-planner/database/migrations/2022_02_06_155450_create_drivers_table.php
+++ convoy-planner/database/migrations/2022_02_06_155450_create_drivers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDriversTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('drivers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('licence_number')->unique();
+            $table->foreign('dispatcher_id')
+                ->references('id')->on('dispatchers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('drivers');
     }
 }
